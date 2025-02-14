b61dec1efd82cef9194d4528ed01779d281b51a3
Ravi <ravi.yadav1983@gmail.com>
2025-02-14 14:07:37 +0530

    Added Terraform code for Azur

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+    required_version = ">= 1.1.0"
+    backend "remote" {
+    organization = "area51coder"
+
+    workspaces {
+      name = "azureterra"
+    }
+  }
+}
+
+provider "azurerm" {
+  client_id       = "33c3ef55-4dc3-44c7-8131-bebc98031f41"
+  client_secret   = "hG.8Q~OOSrtG5e_4J1DMMcdo~2lza2BfIiEknbGX"
+  tenant_id       = "b4329df8-87bd-421e-941c-60801f6c2a51"
+  subscription_id = "a5fdfee4-50cb-4ebc-9681-a8e2a2349fe5"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
